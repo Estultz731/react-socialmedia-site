2ec8a0191c88e44079a17c7945208d5b1e4a209b
Estultz731 <elizablue731@gmail.com>
2019-08-08 12:04:25 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,24 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Nav = () => (
+  <nav>
+    <a className='logo' href='#logo'>
+      <i className='fas fa-bug' />
+    </a>
+    <input type='text' placeholder='Search' name='search' />
+    <button type='submit'>
+      <i className='fas fa-search' />
+    </button>
+    <a className='name' href='#name'>
+      Jane Doe
+    </a>
+    <a className='home' href='#home'>
+      Home
+    </a>
+    <a className='friends' href='#friends'>
+      Find a Friend
+    </a>
+    <a className='create' href='#create'>
+      Create
+    </a>
+    <a className='friend' href='#requests'>
+      <i className='fas fa-users' />
+    </a>
+    <a className='message' href='#messages'>
+      <i className='far fa-comment-dots' />
+    </a>
+    <a className='notify' href='#notify'>
+      <i className='far fa-bell' />
+    </a>
+    <a className='help' href='#help'>
+      <i className='far fa-question-circle' />
+    </a>
+  </nav>
+);
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+    </>
   );
 }
 
